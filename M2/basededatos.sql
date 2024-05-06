de2e36294be62ceff74b383eab51f8259545c815
MCachinero <153191242+MCachienro@users.noreply.github.com>
2024-05-06 21:09:48 +0200

    PLSQL correccion errores

--- M2/basededatos.sql
+++ M2/basededatos.sql
@@ -1,10 +1,10 @@
 CREATE TABLE civilization_stats
 (
   civilization_id NUMBER(10) NOT NULL PRIMARY KEY,
-  name VARCHAR2(50) NOT NULL,
-  wood_amount NUMBER(10) NOT NULL,
-  iron_amount NUMBER(10) NOT NULL,
-  food_amount NUMBER(10) NOT NULL,
+  name VARCHAR2(50),
+  wood_amount NUMBER(10) ,
+  iron_amount NUMBER(10) ,
+  food_amount NUMBER(10) ,
   mana_amount NUMBER(10), 
 
   magicTower_counter NUMBER(10), 
@@ -21,34 +21,41 @@ CREATE TABLE civilization_stats
 CREATE TABLE attack_units_stats
 (
   civilization_id NUMBER(10) NOT NULL, 
-  unit_id VARCHAR2(50) NOT NULL PRIMARY KEY,
-  type ENUM('Swordsman', 'Spearman', 'Crossbow', 'Cannon'), 
+  unit_id VARCHAR2(50) NOT NULL,
+  type_unit VARCHAR2(10), 
   armor NUMBER(10),
   base_damage NUMBER(10), 
   experience NUMBER(10),
   sanctified VARCHAR2(3), 
-  FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id) 
+  FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id),
+  CONSTRAINT pk_civi_unit PRIMARY KEY (civilization_id, unit_id),
+  CONSTRAINT tipesunit CHECK(type_unit IN('Swordsman', 'Spearman', 'Crossbow', 'Cannon'))
 );
 
-CREATE TABLE special_units_Stats
+CREATE TABLE special_units_stats
 (
   civilization_id NUMBER(10) NOT NULL, 
-  unit_id VARCHAR2(50) NOT NULL PRIMARY KEY,
-  type ENUM('Magican', 'Priest'), 
+  unit_id VARCHAR2(50),
+  type_unit VARCHAR2(10), 
   armor NUMBER(10),
   base_damage NUMBER(10), 
   experience NUMBER(10),
-  FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id) 
+  FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id),
+  CONSTRAINT pk_primary_special PRIMARY KEY (civilization_id, unit_id),
+  CONSTRAINT typeunitspecial CHECK(type_unit IN('Magican', 'Priest'))
+
 );
 
 CREATE TABLE defense_unit_stats
 (
   civilization_id NUMBER(10) NOT NULL, 
-  unit_id VARCHAR2(50) NOT NULL PRIMARY KEY,
-  type ENUM('ArrowTower', 'Catapult','RocketLauncherTower'), 
+  unit_id VARCHAR2(50),
+  type_unit VARCHAR2(10), 
   armor NUMBER(10),
   base_damage NUMBER(10), 
   experience NUMBER(10),
   sanctified VARCHAR2(3),
-  FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id) 
+  FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id),
+  CONSTRAINT pk_primary_defense PRIMARY KEY (civilization_id, unit_id),
+  CONSTRAINT type_unit_defense CHECK (type_unit IN('ArrowTower', 'Catapult','RocketLauncherTower'))
 );
