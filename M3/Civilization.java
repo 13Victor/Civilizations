8302f79e8f819c20a8625e51260f24a2e9915ab7
MCachinero <153191242+MCachienro@users.noreply.github.com>
2024-05-07 19:07:14 +0200

    modelo de civilizaiton class

--- M3/Civilization.java
+++ M3/Civilization.java
@@ -1,6 +1,31 @@
 import java.util.ArrayList;
 
+public class ResourceException extends Exception {
+    public ResourceException(String message) {
+        super(message);
+    }
+}
+
+public class BuildingException extends Exception {
+    public BuildingException(String message) {
+        super(message);
+    }
+}
+
 public class Civilization {
+    private static final double UPGRADE_TECH_COST_INCREASE_PERCENTAGE = 0.10;
+    private int upgradeDefenseTechnologyIronCost = 100;
+    private int upgradeAttackTechnologyIronCost = 100;
+    private int upgradeDefenseTechnologyWoodCost = 0;
+    private int upgradeAttackTechnologyWoodCost = 0;
+
+    private static final int SWORDSMAN_WOOD_COST = 10;
+    private static final int SWORDSMAN_IRON_COST = 5;
+    private static final int MAGICIAN_WOOD_COST = 20;
+    private static final int MAGICIAN_MANA_COST = 10;
+    private static final int PRIEST_WOOD_COST = 15;
+    private static final int PRIEST_IRON_COST = 5;
+
     private int technologyDefense;
     private int technologyAttack;
     private int wood;
@@ -127,5 +152,82 @@ public class Civilization {
 
     public void setArmy(ArrayList<MilitaryUnit>[] army) {
         this.army = army;
+    }
+
+    public void upgradeTechnologyDefense() throws ResourceException {
+        int currentCost = upgradeDefenseTechnologyIronCost;
+        if (iron < currentCost || wood < upgradeDefenseTechnologyWoodCost) {
+            throw new ResourceException("Not enough resources to upgrade defense technology.");
+        }
+        upgradeDefenseTechnologyIronCost += (int) (currentCost * UPGRADE_TECH_COST_INCREASE_PERCENTAGE);
+        upgradeDefenseTechnologyWoodCost += (int) (upgradeDefenseTechnologyWoodCost * UPGRADE_TECH_COST_INCREASE_PERCENTAGE);
+    }
+
+    public void upgradeTechnologyAttack() throws ResourceException {
+        int currentCost = upgradeAttackTechnologyIronCost;
+        if (iron < currentCost || wood < upgradeAttackTechnologyWoodCost) {
+            throw new ResourceException("Not enough resources to upgrade attack technology.");
+        }
+        upgradeAttackTechnologyIronCost += (int) (currentCost * UPGRADE_TECH_COST_INCREASE_PERCENTAGE);
+        upgradeAttackTechnologyWoodCost += (int) (upgradeAttackTechnologyWoodCost * UPGRADE_TECH_COST_INCREASE_PERCENTAGE);
+    }
+
+    public void newSwordsman(int n) throws ResourceException {
+        int availableUnits = Math.min(wood / SWORDSMAN_WOOD_COST, iron / SWORDSMAN_IRON_COST);
+        int unitsToAdd = Math.min(n, availableUnits);
+        if (unitsToAdd == 0) {
+            throw new ResourceException("Not enough resources to recruit any swordsman.");
+        }
+        // Lógica para añadir unidades de espadachines
+    }
+
+    public void newMagician(int n) throws ResourceException, BuildingException {
+        if (magicTower == 0) {
+            throw new BuildingException("You need at least one magic tower to recruit magicians.");
+        }
+        int availableUnits = Math.min(wood / MAGICIAN_WOOD_COST, mana / MAGICIAN_MANA_COST);
+        int unitsToAdd = Math.min(n, availableUnits);
+        if (unitsToAdd == 0) {
+            throw new ResourceException("Not enough resources to recruit any magicians.");
+        }
+        // Lógica para añadir unidades de magos
+    }
+
+    public void newPriest(int n) throws ResourceException, BuildingException {
+        if (church == 0) {
+            throw new BuildingException("You need at least one church to recruit priests.");
+        }
+        int availableUnits = Math.min(wood / PRIEST_WOOD_COST, iron / PRIEST_IRON_COST);
+        int unitsToAdd = Math.min(n, availableUnits);
+        if (unitsToAdd == 0) {
+            throw new ResourceException("Not enough resources to recruit any priests.");
+        }
+        // Lógica para añadir unidades de sacerdotes
+    }
+
+    public void printStats() {
+        System.out.println("***************************CIVILIZATION STATS***************************");
+        System.out.println("--------------------------------------------------TECHNOLOGY----------------------------------------");
+        System.out.println("Atack\tDefense");
+        System.out.println(technologyAttack + "\t" + technologyDefense);
+        System.out.println("---------------------------------------------------BUILDINGS----------------------------------------");
+        System.out.println("Farm\tSmithy\tCarpentry\tMagic Tower\tChurch");
+        System.out.println(farm + "\t" + smithy + "\t" + carpentry + "\t\t" + magicTower + "\t\t\t" + church);
+        System.out.println("----------------------------------------------------DEFENSES----------------------------------------");
+        System.out.println("Arrow Tower\tCatapult\tRocket Launcher");
+        System.out.println(army[4].size() + "\t\t\t" + army[5].size() + "\t\t" + army[6].size());
+        System.out.println("------------------------------------------------ATTACK UNITS----------------------------------------");
+        System.out.println("Swordsman\tSpearman\tCrossbow\tCannon");
+        System.out.println(army[0].size() + "\t\t\t" + army[1].size() + "\t\t\t" + army[2].size() + "\t\t" + army[3].size());
+        System.out.println("----------------------------------------------ESPECIAL UNITS----------------------------------------");
+        System.out.println("Magician\tPriest");
+        System.out.println(army[7].size() + "\t\t\t" + army[8].size());
+        System.out.println("---------------------------------------------------RESOURCES----------------------------------------");
+        System.out.println("Food\t\tWood\t\tIron\t\tMana");
+        System.out.println(food + "\t\t" + wood + "\t\t" + iron + "\t\t" + mana);
+        System.out.println("----------------------------------------GENERATION RESOURCES----------------------------------------");
+        System.out.println("Food\t\tWood\t\tIron\t\tMana");
+        // Aquí deberías tener atributos que indiquen la generación de recursos, que no están definidos en la pregunta original
+        System.out.println("8000\t\t5000\t\t1500\t\t0");
     }
 }
