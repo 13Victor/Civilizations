a3db86aa331d66e512518a8fca764271e22513df
CronoxHG <daniel.hirsch.garcia@gmail.com>
2024-05-07 21:22:30 +0200

    avances

---
+++ M3/demo/src/main/java/com/project/AttackUnit.java
@@ -0,0 +1,12 @@
+package com.project;
+
+public abstract class AttackUnit implements MilitaryUnit, Variables{
+    
+    protected int armor ;
+    protected int initialArmor;
+    protected int baseDamage;
+    protected int experience;
+    protected boolean sanctified;
+
+}
+
---
+++ M3/demo/src/main/java/com/project/Swordsman.java
@@ -0,0 +1,13 @@
+package com.project;
+
+public class Swordsman extends AttackUnit{
+  
+    public final static int ARMOR_SWORDSMAN = 400;
+    public final static int BASE_DAMAGE_SWORDSMAN = 80;
+
+    public Swordsman(int defenseTechnologyLevel, int attackTechnologyLevel){
+        calculate
+    }
+
+
+}
