1c10134672ff2d23807c815baab1175fa4cad843
MCachinero <mcachinerobaranda.cf@iesesteveterradas.cat>
2024-05-07 21:08:52 +0200

    magical

---
+++ M3/demo/src/main/java/com/project/magical.java
@@ -0,0 +1,11 @@
+package com.project;
+
+public class magical extends specialunit{
+    
+    public magical(int initialArmor, int baseDamage) {
+        this.initialArmor = initialArmor;
+        this.armor = 0; 
+        this.baseDamage = baseDamage;
+        this.experience = 0;
+    }
+}
