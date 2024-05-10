61a3551f4054851749727457e67023b63b6661e9
MCachinero <mcachinerobaranda.cf@iesesteveterradas.cat>
2024-05-10 17:55:20 +0200

    magician unit terminado

---
+++ M3/demo/src/main/java/com/project/magician.java
@@ -0,0 +1,89 @@
+package com.project;
+
+public class magician extends specialunit{
+
+    public final int BASE_DAMAGE_MAGICIAN = 3000;
+
+    public magician(int initialArmor, int attackmagicianlevel) {
+        this.initialArmor = initialArmor;
+        this.armor = 0; 
+        this.baseDamage = BASE_DAMAGE_MAGICIAN + (int)(attackmagicianlevel*PLUS_ATTACK_MAGICIAN_BY_TECHNOLOGY)*BASE_DAMAGE_MAGICIAN/100;;
+        this.experience = 0;
+    }
+
+    public magician(){
+        this.armor = 0;
+        this.initialArmor = this.armor;
+        this.baseDamage = BASE_DAMAGE_MAGICIAN;
+        this.experience = 0;
+    }
+
+    @Override
+    public int attack(){
+        int calculatedDamage = (int)(baseDamage + baseDamage * experience * PLUS_ATTACK_UNIT_PER_EXPERIENCE_POINT / 100);
+        return calculatedDamage;  
+    }
+
+    @Override
+    public void takeDamage(int receivedDamage) {
+        armor -= receivedDamage;
+    }
+
+    @Override
+    public int getActualArmor() {
+        return (int)(armor+initialArmor*experience*PLUS_ARMOR_UNIT_PER_EXPERIENCE_POINT/100);
+    }
+
+    @Override
+    public int getFoodCost(){
+        return FOOD_COST_MAGICIAN;
+    }
+
+    @Override
+    public int getWoodCost(){
+        return WOOD_COST_MAGICIAN;
+    }
+
+    @Override
+    public int getIronCost(){
+        return IRON_COST_MAGICIAN;
+    }
+
+    @Override
+    public int getManaCost(){
+        return MANA_COST_MAGICIAN;
+    }
+
+    @Override
+    public int getChanceGeneratinWaste(){
+        return CHANCE_GENERATNG_WASTE_MAGICIAN;
+    }
+
+    @Override
+    public int getChanceAttackAgain(){
+        return CHANCE_ATTACK_AGAIN_MAGICIAN;
+    }
+
+    @Override
+    public void resetArmor() {
+        armor = initialArmor;
+    }
+
+    @Override
+    public void setExperience(int exp) {
+        experience = exp;
+    }
+
+    @Override
+    public int getExperience() {
+        return experience;
+    }
+
+    @Override
+    public UnitTypes getType() {
+        return UnitTypes.CANNON;
+    }
+
+}
+    
+}
--- M3/demo/src/main/java/com/project/specialunit.java
+++ M3/demo/src/main/java/com/project/specialunit.java
@@ -1,6 +1,6 @@
 package com.project;
 
-public abstract class specialunit {
+public abstract class specialunit implements MilitaryUnit, Variables{
 
     protected int armor = 0;
     protected int initialArmor = 0;
