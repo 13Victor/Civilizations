df44b88b6d01ce5f217dee1f049273baa3d58720
CronoxHG <daniel.hirsch.garcia@gmail.com>
2024-05-10 16:31:57 +0200

    Clase DefenseUnit
    Creacion de la clase DefenseUnit

---
+++ M3/demo/src/main/java/com/project/DefenseUnit.java
@@ -0,0 +1,11 @@
+package com.project;
+
+public abstract class DefenseUnit implements MilitaryUnit, Variables{
+    
+    protected int armor ;
+    protected int initialArmor;
+    protected int baseDamage;
+    protected int experience;
+    protected boolean sanctified;
+
+}
