72a495188fe25ef522f984902e6ecc1d891760ca
CronoxHG <daniel.hirsch.garcia@gmail.com>
2024-05-13 20:09:47 +0200

    Creacion de la clase Utype
    ESta clase se utiliza para hacer un enum de las unidades  para mejorar la legibilidad y mantenibilidad del codigo

---
+++ M3/demo/src/main/java/com/project/uType.java
@@ -0,0 +1,14 @@
+package com.project;
+
+public enum uType {
+    Swordsman,
+    Spearman,
+    Crossbow,
+    Cannon,
+    ArrowTower,
+    Catapult,
+    RocketLauncherTower,
+    Magician,
+    Priest
+}
+
