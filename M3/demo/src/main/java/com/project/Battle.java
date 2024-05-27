2e96b6d518c80f9ceb881917125a5565cd08cc54
CronoxHG <daniel.hirsch.garcia@gmail.com>
2024-05-28 01:43:46 +0200

    Corrección de la clase battle
    Se ha corregido la clase battle , ademas se han implementado las funciones determineBattleOutcome, calculateResourcesAcquired,
    getUnitResourceCost y
    generateDetailedReport

--- M3/demo/src/main/java/com/project/Battle.java
+++ M3/demo/src/main/java/com/project/Battle.java
@@ -3,9 +3,9 @@ package com.project;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Battle {
+public class Battle implements Variables {
 
-    private ArrayList<MilitaryUnit>[] armies;
+    private ArrayList<MilitaryUnit>[][] armies;
     private StringBuilder battleDevelopment;
     private int[][] initialCostFleet;
     private int initialNumberUnitsCivilization;
@@ -13,7 +13,7 @@ public class Battle {
     private int[] wasteWoodIron;
     private int enemyDrops;
     private int civilizationDrops;
-    private int[][] resourcesLooses;
+    private int[][] resourcesLosses;
     private int[][] initialArmies;
     private int[] actualNumberUnitsCivilization;
     private int[] actualNumberUnitsEnemy;
@@ -22,31 +22,31 @@ public class Battle {
     private int woodAcquired;
     private int ironAcquired;
     private boolean win;
-    private ArrayList<Integer> civilizationLoses;
-    private ArrayList<Integer> enemyLoses;
-    private ArrayList<MilitaryUnit> civilizationArmy;
-    private ArrayList<MilitaryUnit> enemyArmy;
+    private ArrayList<Integer> civilizationLosses;
+    private ArrayList<Integer> enemyLosses;
+    private ArrayList<MilitaryUnit>[] civilizationArmy;
+    private ArrayList<MilitaryUnit>[] enemyArmy;
     private String detailedReport;
 
     @SuppressWarnings("unchecked")
-    public Battle(ArrayList<MilitaryUnit> civilizationArmy, ArrayList<MilitaryUnit> enemyArmy) {
+    public Battle(ArrayList<MilitaryUnit>[] civilizationArmy, ArrayList<MilitaryUnit>[] enemyArmy) {
         this.civilizationArmy = civilizationArmy;
         this.enemyArmy = enemyArmy;
-        this.armies = new ArrayList[2];
+        this.armies = (ArrayList<MilitaryUnit>[][]) new ArrayList[2][];
         this.armies[0] = civilizationArmy;
         this.armies[1] = enemyArmy;
         this.battleDevelopment = new StringBuilder();
         this.initialCostFleet = new int[2][3];
         this.wasteWoodIron = new int[2];
-        this.resourcesLooses = new int[2][4];
+        this.resourcesLosses = new int[2][4];
         this.initialArmies = new int[2][9];
         this.actualNumberUnitsCivilization = new int[9];
         this.actualNumberUnitsEnemy = new int[9];
         this.random = new Random();
-        this.civilizationLoses = new ArrayList<>();
-        this.enemyLoses = new ArrayList<>();
+        this.civilizationLosses = new ArrayList<>();
+        this.enemyLosses = new ArrayList<>();
         initInitialArmies();
-        updateResourcesLooses();
+        updateResourcesLosses();
     }
 
     private void initInitialArmies() {
@@ -66,10 +66,10 @@ public class Battle {
         return total;
     }
 
-    private void updateResourcesLooses() {
+    private void updateResourcesLosses() {
         for (int i = 0; i < 4; i++) {
-            resourcesLooses[0][i] = 0;
-            resourcesLooses[1][i] = 0;
+            resourcesLosses[0][i] = 0;
+            resourcesLosses[1][i] = 0;
         }
     }
 
@@ -144,11 +144,75 @@ public class Battle {
     }
 
     private void determineBattleOutcome() {
-        // Determina el resultado de la batalla
-        // Actualiza woodAcquired, ironAcquired, win, civilizationLoses, enemyLoses, detailedReport
+        // Inicializa las pérdidas
+        civilizationLosses.clear();
+        enemyLosses.clear();
+    
+        // Calcula las pérdidas de la civilización
+        for (int i = 0; i < initialArmies[0].length; i++) {
+            int losses = initialArmies[0][i] - (i < civilizationArmy.length ? civilizationArmy[i].size() : 0);
+            civilizationLosses.add(losses);
+        }
+    
+        // Calcula las pérdidas del enemigo
+        for (int i = 0; i < initialArmies[1].length; i++) {
+            int losses = initialArmies[1][i] - (i < enemyArmy.length ? enemyArmy[i].size() : 0);
+            enemyLosses.add(losses);
+        }
+    
+        // Calcula recursos adquiridos
+        woodAcquired = calculateResourcesAcquired("wood");
+        ironAcquired = calculateResourcesAcquired("iron");
+    
+        // Determina si ganó la civilización
+        win = resourcesLosses[0][3] < resourcesLosses[1][3];
+    
+        // Genera el reporte detallado
+        generateDetailedReport();
     }
+    
+    private int calculateResourcesAcquired(String resourceType) {
+        int resources = 0;
+        for (int i = 0; i < civilizationLosses.size(); i++) {
+            resources += getUnitResourceCost(i, resourceType) * civilizationLosses.get(i) * PERCENTATGE_WASTE / 100;
+        }
+        for (int i = 0; i < enemyLosses.size(); i++) {
+            resources += getUnitResourceCost(i, resourceType) * enemyLosses.get(i) * PERCENTATGE_WASTE / 100;
+        }
+        return resources;
+    }
+    
+    private int getUnitResourceCost(int unitIndex, String resourceType) {
+        switch (resourceType) {
+            case "wood":
+                return Variables.WOOD_COST_UNITS[unitIndex];
+            case "iron":
+                return Variables.IRON_COST_UNITS[unitIndex];
+            default:
+                return 0;
+        }
+    }
+    
+    private void generateDetailedReport() {
+        StringBuilder report = new StringBuilder();
+        report.append("Resumen de la Batalla:\n");
+        report.append("Pérdidas de la Civilización:\n");
+        for (int i = 0; i < civilizationLosses.size(); i++) {
+            report.append("Unidad ").append(i).append(": ").append(civilizationLosses.get(i)).append("\n");
+        }
+        report.append("Pérdidas del Enemigo:\n");
+        for (int i = 0; i < enemyLosses.size(); i++) {
+            report.append("Unidad ").append(i).append(": ").append(enemyLosses.get(i)).append("\n");
+        }
+        report.append("Recursos adquiridos:\n");
+        report.append("Madera: ").append(woodAcquired).append("\n");
+        report.append("Hierro: ").append(ironAcquired).append("\n");
+        report.append("Resultado de la batalla: ").append(win ? "Victoria" : "Derrota").append("\n");
+        detailedReport = report.toString();
+    }
+    
 
-    // Getters and setters for new fields
+    // Getters y setters
     public int getNumBattle() {
         return numBattle;
     }
@@ -181,36 +245,138 @@ public class Battle {
         this.win = win;
     }
 
-    public ArrayList<Integer> getCivilizationLoses() {
-        return civilizationLoses;
+    public ArrayList<Integer> getCivilizationLosses() {
+        return civilizationLosses;
     }
 
-    public void setCivilizationLoses(ArrayList<Integer> civilizationLoses) {
-        this.civilizationLoses = civilizationLoses;
+    public void setCivilizationLosses(ArrayList<Integer> civilizationLosses) {
+        this.civilizationLosses = civilizationLosses;
     }
 
-    public ArrayList<Integer> getEnemyLoses() {
-        return enemyLoses;
+    public ArrayList<Integer> getEnemyLosses() {
+        return enemyLosses;
     }
 
-    public void setEnemyLoses(ArrayList<Integer> enemyLoses) {
-        this.enemyLoses = enemyLoses;
+    public void setEnemyLosses(ArrayList<Integer> enemyLosses) {
+        this.enemyLosses = enemyLosses;
     }
 
-    public ArrayList<MilitaryUnit> getCivilizationArmy() {
+    public ArrayList<MilitaryUnit>[] getCivilizationArmy() {
         return civilizationArmy;
     }
 
-    public void setCivilizationArmy(ArrayList<MilitaryUnit> civilizationArmy) {
+    public void setCivilizationArmy(ArrayList<MilitaryUnit>[] civilizationArmy) {
         this.civilizationArmy = civilizationArmy;
+        this.armies[0] = civilizationArmy;
     }
 
-    public ArrayList<MilitaryUnit> getEnemyArmy() {
+    public ArrayList<MilitaryUnit>[] getEnemyArmy() {
         return enemyArmy;
     }
 
-    public void setEnemyArmy(ArrayList<MilitaryUnit> enemyArmy) {
+    public void setEnemyArmy(ArrayList<MilitaryUnit>[] enemyArmy) {
         this.enemyArmy = enemyArmy;
+        this.armies[1] = enemyArmy;
+    }
+
+    public ArrayList<MilitaryUnit>[][] getArmies() {
+        return armies;
+    }
+
+    public void setArmies(ArrayList<MilitaryUnit>[][] armies) {
+        this.armies = armies;
+    }
+
+    public void setBattleDevelopment(StringBuilder battleDevelopment) {
+        this.battleDevelopment = battleDevelopment;
+    }
+
+    public int[][] getInitialCostFleet() {
+        return initialCostFleet;
+    }
+
+    public void setInitialCostFleet(int[][] initialCostFleet) {
+        this.initialCostFleet = initialCostFleet;
+    }
+
+    public int getInitialNumberUnitsCivilization() {
+        return initialNumberUnitsCivilization;
+    }
+
+    public void setInitialNumberUnitsCivilization(int initialNumberUnitsCivilization) {
+        this.initialNumberUnitsCivilization = initialNumberUnitsCivilization;
+    }
+
+    public int getInitialNumberUnitsEnemy() {
+        return initialNumberUnitsEnemy;
+    }
+
+    public void setInitialNumberUnitsEnemy(int initialNumberUnitsEnemy) {
+        this.initialNumberUnitsEnemy = initialNumberUnitsEnemy;
+    }
+
+    public int[] getWasteWoodIron() {
+        return wasteWoodIron;
+    }
+
+    public void setWasteWoodIron(int[] wasteWoodIron) {
+        this.wasteWoodIron = wasteWoodIron;
+    }
+
+    public int getEnemyDrops() {
+        return enemyDrops;
+    }
+
+    public void setEnemyDrops(int enemyDrops) {
+        this.enemyDrops = enemyDrops;
+    }
+
+    public int getCivilizationDrops() {
+        return civilizationDrops;
+    }
+
+    public void setCivilizationDrops(int civilizationDrops) {
+        this.civilizationDrops = civilizationDrops;
+    }
+
+    public int[][] getResourcesLosses() {
+        return resourcesLosses;
+    }
+
+    public void setResourcesLosses(int[][] resourcesLosses) {
+        this.resourcesLosses = resourcesLosses;
+    }
+
+    public int[][] getInitialArmies() {
+        return initialArmies;
+    }
+
+    public void setInitialArmies(int[][] initialArmies) {
+        this.initialArmies = initialArmies;
+    }
+
+    public int[] getActualNumberUnitsCivilization() {
+        return actualNumberUnitsCivilization;
+    }
+
+    public void setActualNumberUnitsCivilization(int[] actualNumberUnitsCivilization) {
+        this.actualNumberUnitsCivilization = actualNumberUnitsCivilization;
+    }
+
+    public int[] getActualNumberUnitsEnemy() {
+        return actualNumberUnitsEnemy;
+    }
+
+    public void setActualNumberUnitsEnemy(int[] actualNumberUnitsEnemy) {
+        this.actualNumberUnitsEnemy = actualNumberUnitsEnemy;
+    }
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
     }
 
     public String getDetailedReport() {
