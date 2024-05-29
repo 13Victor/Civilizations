56c57f03d5d447b037ab2b3e4cf0a9b2b699f08e
Victor Valero <vvalerolopez.eb@iesesteveterradas.cat>
2024-05-30 01:01:32 +0200

    web 99%

--- M3/src/main/java/com/project/ArrowTower.java
+++
@@ -1,9 +0,0 @@
-package com.project;
-
-public class ArrowTower extends DefenseUnit {
-
-    public ArrowTower( int armor, int baseDamage) {
-        
-    }
-    
-}
--- M3/src/main/java/com/project/DefenseUnit.java
+++
@@ -1,9 +0,0 @@
-package com.project;
-
-public abstract class DefenseUnit implements MilitaryUnit, Variables {
-    protected int armor;
-    protected int initialArmor;
-    protected int baseDamage;
-    protected int experience;
-    protected boolean sanctified;
-}
